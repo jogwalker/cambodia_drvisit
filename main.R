f22905cc60292b9d22d1bef25ae8f8b8e74c9a8f
JG Walker <jw12513@it017395.local>
2016-12-05 16:41:15 +0700

    added second plot by type of visit

--- main.R
+++ main.R
@@ -4,10 +4,12 @@
 library(lubridate)
 library(Hmisc)
 library(ggplot2)
+library(tidyr)
+library(dplyr)
 
 startdate <- as.Date("2016-12-01")
 runtime <- 2 # years
-elisaperday <- 50
+elisaperday <- 30
 elisayield <- 0.6
 elisawait <- 7 # days
 PCRyield <- 0.75
@@ -19,11 +21,15 @@ eligyield <- 0.9
 treat24weeks <- 0.2 # proportion on 24 week treatment, rest on 12 weeks
 waitbaseline <- 28 # how long between PCR+ appointment and Day 0
 
+reducescreening <- 84 # after this many days
+reducescreeningby <- 0.5
+
 days <- seq(startdate,startdate+years(runtime),by=1)
 
 dat <- data.frame(date = as.Date(days),wday = wday(days))
 dat$ELISA <- 0
 dat$ELISA[dat$wday <= 5] <- elisaperday
+dat$ELISA[dat$date >= (startdate+reducescreening)] <- dat$ELISA[dat$date >= (startdate+reducescreening)]*reducescreeningby
 # could draw from poisson instead of multiplying
 dat$PCR <- Lag(dat$ELISA*elisayield,elisawait)
 dat$baseline <- Lag(dat$PCR*PCRyield,PCRwait)
@@ -37,12 +43,21 @@ dat$M4 <- Lag(dat$M3*treat24weeks,28)
 dat$M5 <- Lag(dat$M4,28)
 dat$M6 <- Lag(dat$M5,28) # end of treatment visit for 24 week treatment
 dat$SVR12 <- rowSums(cbind(Lag(dat$M3,84),Lag(dat$M6,84)),na.rm=T)
-dat$SVR12result <- Lag(dat$SVR12,28)
+dat$SVR12result <- Lag(dat$SVR12,28) # for now assuming no retreatment
 
 dat[is.na(dat)] <- 0
 dat$screeningvisit <- rowSums(cbind(dat$ELISA,dat$PCR))
-dat$drvisit <- rowSums(cbind(dat[,5:12]))
+dat$drvisit <- rowSums(cbind(dat[,5:16]))
+dat$baselinevisit <- rowSums(cbind(dat[,5:6]))
+dat$treatmentvisit <- rowSums(cbind(dat[,7:14]))
+dat$followupvisit <- rowSums(cbind(dat[,15:16]))
 dat[dat==0] <- NA
 
 # plot
-ggplot(dat,aes(x=date,y=drvisit)) + geom_line()
+dat2 <- dat %>% gather("visit_type","n",17:18)
+ggplot(dat2,aes(x=date,y=n,color=visit_type)) + geom_line() 
+
+dat3 <- dat %>% gather("visit_type","n",17:21)
+ggplot(dat3,aes(x=date,y=n,color=visit_type)) + geom_line() 
+
+
