26708de5797c6900f51d1906e50d64c0dcfba4cc
JG Walker <jw12513@it017395.local>
2016-12-06 15:59:41 +0700

    setting up server.R and trying to get reactive button to work but still needs more time

--- server.R
+++ server.R
@@ -5,30 +5,62 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 
-function(input, output) {
+
+getdat <- function(dates,elisaperday,elisaperday2,reducescreening,elisayield,elisawait,PCRyield,PCRwait,waitlist,ltfuF0F1,waitF0F1,waitbaseline,eligyield) {
+  days <- seq(dates[1],dates[2],by=1)
   
-  dataset <- reactive({
-    diamonds[sample(nrow(diamonds), input$sampleSize),]
+  dat <- data.frame(date = as.Date(days),wday = wday(days))
+  dat$ELISA <- 0
+  dat$ELISA[dat$wday <= 5] <- elisaperday
+  dat$ELISA[dat$date >= (reducescreening)] <- elisaperday2
+  dat$PCR <- Lag(dat$ELISA*elisayield,elisawait*7)
+  dat$baseline <- Lag(dat$PCR*PCRyield,PCRwait*7)
+  dat$reassess_F0F1 <- Lag(dat$baseline*waitlist*(1-ltfuF0F1),waitF0F1*7)
+  dat$day0 <- rowSums(cbind(Lag(dat$baseline*(1-waitlist),waitbaseline*7), Lag(dat$reassess_F0F1,waitbaseline*7)),na.rm=T)
+  dat$day14 <- Lag(dat$day0*eligyield,14)
+  dat$M1 <- Lag(dat$day14,14)
+  dat$M2 <- Lag(dat$M1,28)
+  dat$M3 <- Lag(dat$M2,28) # end of treatment visit for 12 week treatment
+  dat$M4 <- Lag(dat$M3*treat24weeks,28)
+  dat$M5 <- Lag(dat$M4,28)
+  dat$M6 <- Lag(dat$M5,28) # end of treatment visit for 24 week treatment
+  dat$SVR12 <- rowSums(cbind(Lag(dat$M3,84),Lag(dat$M6,84)),na.rm=T)
+  dat$SVR12result <- Lag(dat$SVR12,28) # for now assuming no retreatment
+  
+  dat[is.na(dat)] <- 0
+  dat$screeningvisit <- rowSums(cbind(dat$ELISA,dat$PCR))
+  dat$drvisit <- rowSums(cbind(dat[,5:16]))
+  dat$baselinevisit <- rowSums(cbind(dat[,5:6]))
+  dat$treatmentvisit <- rowSums(cbind(dat[,7:14]))
+  dat$followupvisit <- rowSums(cbind(dat[,15:16]))
+  # how many patients are on treatment at once?
+  dat$starttreatment <- dat$day0*eligyield
+  dat$treated <- cumsum(dat$starttreatment)
+  dat$endtreatment <- cumsum(dat$M3)*(1-treat24weeks) + cumsum(dat$M6)
+  dat$ontreatment <- dat$treated - dat$endtreatment
+  dat[dat == 0] <-  NA
+  return(dat)
+}
+
+shinyServer(
+  function(input, output) {
+  
+  dat <- eventReactive(input$run, {
+    getdat(input$dates,input$elisaperday,input$elisaperday2,input$reducescreening,input$elisayield,input$elisawait,input$PCRyield,input$PCRwait,input$waitlist,input$ltfuF0F1,input$waitF0F1,input$waitbaseline,input$eligyield)
   })
   
-  output$plot <- renderPlot({
-    
-    p <- ggplot(dataset(), aes_string(x=input$x, y=input$y)) + geom_point()
-    
-    if (input$color != 'None')
-      p <- p + aes_string(color=input$color)
-    
-    facets <- paste(input$facet_row, '~', input$facet_col)
-    if (facets != '. ~ .')
-      p <- p + facet_grid(facets)
-    
-    if (input$jitter)
-      p <- p + geom_jitter()
-    if (input$smooth)
-      p <- p + geom_smooth()
+  output$plot1 <- renderPlot({
+    p <- dat() %>% gather("visit_type","n",17:21) %>% ggplot(aes(x=date,y=n,color=visit_type)) + geom_line() + theme_bw()
     
     print(p)
     
-  }, height=700)
+  })
   
-}
+  output$plot2 <- renderPlot({
+    p <- ggplot(dat(),aes(x=date,y=ontreatment)) + geom_line() + theme_bw()
+    print(p)
+    
+  })
+  
+  }
+)
--- ui.R
+++ ui.R
@@ -7,34 +7,16 @@ library(dplyr)
 
 fluidPage(
   
-  # startdate <- as.Date("2016-12-01")
-  # runtime <- 3 # years
-  # elisaperday <- 30
-  # elisayield <- 0.6
-  # elisawait <- 7 # days
-  # PCRyield <- 0.75
-  # PCRwait <- 14 # days
-  # waitlist <- 0.5
-  # waitF0F1 <- 182 # days
-  # ltfuF0F1 <- 0.1 #
-  # eligyield <- 0.9
-  # treat24weeks <- 0.2 # proportion on 24 week treatment, rest on 12 weeks
-  # waitbaseline <- 28 # how long between PCR+ appointment and Day 0
-  # 
-  # reducescreening <- 84 # after this many days
-  # reducescreeningby <- 0.5
-  
-  
   titlePanel("MSF HCV Appointment Simulation"),
   
   sidebarPanel(
     
-    dateRangeInput("dates", label = h3("Start and end date of simulation")),
+    dateRangeInput("dates", label = "Start and end date of simulation",start="2017-01-01", end="2019-01-01"),
     
     sliderInput('elisaperday', 'Antibody tests/day', min=1, max=200,
                 value=20, step=1, round=0),
     
-    dateInput('reducescreening','Date from which screening rate changes')
+    dateInput('reducescreening','Date from which screening rate changes'),
     
     sliderInput('elisaperday2', 'Antibody tests/day phase 2', min=0, max=200,
                 value=20, step=1, round=0),
@@ -47,7 +29,7 @@ fluidPage(
     
     sliderInput('PCRwait', 'Waiting time for PCR test result (weeks)', min=0, max=52,  value=2, step=1, round=0),
     
-    sliderInput('PCRwait', 'Waiting time for treatment initiation (weeks)', min=0, max=52,  value=4, step=1, round=0),
+    sliderInput('waitbaseline', 'Waiting time for treatment initiation (weeks)', min=0, max=52,  value=4, step=1, round=0),
     
     sliderInput('eligyield', 'Proportion of chronic infections eligible for treatment', min=0, max=1,  value=0.9, step=0.01, round=0),
     
@@ -60,12 +42,12 @@ fluidPage(
     
     sliderInput('waitlist', 'Proportion of patients on waiting list LTFU', min=0, max=1,  value=0.1, step=0.01, round=0),
     
-    submitButton("Run Simulation")
+    actionButton("run","Run Simulation")
   ),
   
   mainPanel(
-    plotOutput('plot1',width=400,height=400),
-    plotOutput('plot2',width=400,height=400)
+    plotOutput('plot1',width=700,height=400),
+    plotOutput('plot2',width=600,height=400)
     # text of treatment numbers or display on plot?
   )
 )
